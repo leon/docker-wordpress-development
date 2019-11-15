577ce7dd9374125fc340f54119b7408c499b09ee
Leon Radley <leon@radley.se>
2019-11-15 15:52:06 +0100

    update to wordpress 5.3 and php 7.3

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@ set -eo pipefail
 
 # Pull new versions of deps
 docker pull nginx:mainline-alpine
-docker pull wordpress:5.2-php7.2-fpm
-docker pull php:7.2-fpm
+docker pull wordpress:5.3-php7.3-fpm
+docker pull php:7.3-fpm
 
 # Build wordpress image
 docker build wordpress -t wec360/wordpress-development
