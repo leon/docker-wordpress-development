6c07751cdd484b70d70a1c9f6dc9e5475128f416
Leon Radley <leon@wec360.se>
2020-06-01 13:42:58 +0200

    update to wordpress 5.4

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 # Pull new versions of deps
 docker pull nginx:mainline-alpine
-docker pull wordpress:5.3-php7.3-fpm
+docker pull wordpress:5.4-php7.3-fpm
 docker pull php:7.3-fpm
 
 # Build wordpress image
